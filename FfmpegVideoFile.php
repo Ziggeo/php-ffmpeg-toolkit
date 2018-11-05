3f0d94667c77270cf50136acd10b04904fd894e5
Oliver Friedmann <oliver.friedmann@gmail.com>
2018-11-05 15:30:34 -0500

    Formatting

--- FfmpegVideoFile.php
+++ FfmpegVideoFile.php
@@ -4,112 +4,113 @@ require_once(dirname(__FILE__) . "/FfmpegVideoCodecs.php");
 require_once(dirname(__FILE__) . "/FfmpegVideoTranscoding.php");
 
 Class FfmpegVideoFile {
-	
-	private $filename;
-	private $ffmpeg;
-	private $video;
-	private $movie;
-	private $rotation;
-	
-	function __construct($name, $options = array()) {
-		$this->filename = $name;
-		$ffmpeg_binary = "ffmpeg";
+
+    private $filename;
+    private $ffmpeg;
+    private $video;
+    private $movie;
+    private $rotation;
+
+    function __construct($name, $options = array()) {
+        $this->filename = $name;
+        $ffmpeg_binary = "ffmpeg";
         if (FfmpegVideoTranscoding::$ffmpeg_binary)
             $ffmpeg_binary = FfmpegVideoTranscoding::$ffmpeg_binary;
-		$this->movie = new FFmpegMovie($name, NULL, $ffmpeg_binary);
-		$config = array();
+        $this->movie = new FFmpegMovie($name, NULL, $ffmpeg_binary);
+        $config = array();
         if (FfmpegVideoTranscoding::$ffmpeg_binary)
             $config["ffmpeg.binaries"] = array(FfmpegVideoTranscoding::$ffmpeg_binary);
         if (FfmpegVideoTranscoding::$ffprobe_binary)
             $config["ffprobe.binaries"] = array(FfmpegVideoTranscoding::$ffprobe_binary);
-		$this->ffmpeg = FFMpeg\FFMpeg::create($config);
-		$this->video = $this->ffmpeg->open($name);
-		$this->rotation = @$options["rotate_add"] ? $options["rotate_add"] : 0;
+        $this->ffmpeg = FFMpeg\FFMpeg::create($config);
+        $this->video = $this->ffmpeg->open($name);
+        $this->rotation = @$options["rotate_add"] ? $options["rotate_add"] : 0;
         $this->autorotate = @$options["autorotate"] ? $options["autorotate"] : FALSE;
-		if (@$options["rotation"]) {
-			try {
-				$this->rotation += FfmpegVideoTranscoding::getRotation($name);
-			} catch (Exception $e) {}
-		}
-	}
-	
-	function getRotation() {
-		return $this->rotation;
-	}
-	
-	function hasVideo() {
-		return $this->movie->hasVideo();
-	}
-	
-	function hasAudio() {
-		return $this->movie->hasAudio();
-	}
+        if (@$options["rotation"]) {
+            try {
+                $this->rotation += FfmpegVideoTranscoding::getRotation($name);
+            } catch (Exception $e) {
+            }
+        }
+    }
 
-	function getDuration() {
-		return $this->movie->getDuration();
-	}
-	
-	function getWidth() {
-		return $this->rotation % 180 == 0 ? $this->movie->getFrameWidth() : $this->movie->getFrameHeight();
-	}
+    function getRotation() {
+        return $this->rotation;
+    }
 
-	function getHeight() {
-		return $this->rotation % 180 == 0 ? $this->movie->getFrameHeight() : $this->movie->getFrameWidth();
-	}
+    function hasVideo() {
+        return $this->movie->hasVideo();
+    }
 
-	function getVideoCodec() {
-		return $this->movie->getVideoCodec();
-	}
-	
-	function getAudioCodec() {
-		return $this->movie->getAudioCodec();
-	}
-	
-	private function getTempFileName() {
-		return tempnam(sys_get_temp_dir(), "");
-	}
-	
-	function saveImageBySecond($filename = NULL, $seconds = 0, $extension = "png", $safeRevertToZero = FALSE) {
-		$filename = $filename == NULL ? $this->getTempFileName() . "." . $extension : $filename;
-		touch($filename);
-		$frameCount = $this->movie->getFrameCount();
-		if ($frameCount > 0) {
-			$frameDuration = $this->getDuration() / $frameCount;
-			if (ceil($seconds / $frameDuration) >= $frameCount)
-				$seconds = ($frameCount-1) * $frameDuration;
-		}
-		$frame = $this->video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($seconds));
-		if (@$this->rotation && !$this->autorotate)
-			$frame->addFilter(new RotationFrameFilter($this->rotation));
-		$frame->save($filename);
-		if ($safeRevertToZero && !is_file($filename)) {
-			$frame = $this->video->frame(0);
-			if (@$this->rotation && !$this->autorotate)
-				$frame->addFilter(new RotationFrameFilter($this->rotation));
-			$frame->save($filename);
-		}
-		return $filename;
-	}
-	
-	function saveImageByPercentage($filename = NULL, $percentage = 0, $extension = "png", $safeRevertToZero = FALSE) {
-		return $this->saveImageBySecond($filename, $percentage * $this->getDuration(), $extension, $safeRevertToZero);
-	}
-	
-	function saveAudio($filename = NULL, $extension = "wav") {
-		$filename = $filename == NULL ? $this->getTempFileName() . "." . $extension : $filename;
-		touch($filename);
-		$audio = new FFMpeg\Media\Audio($this->filename, $this->ffmpeg->getFFMpegDriver(), $this->ffmpeg->getFFProbe());
-		$audio->save(new NullAudio(), $filename);
-		return $filename;
-	}
-	
-	function getVideoType($filename = NULL) {
-		return FfmpegVideoCodecs::videoTypeByCodecAndFileName($this->getVideoCodec(), @$filename ? $filename : $this->filename);
-	}
-	
-	function getVideoSubType() {
-		return FfmpegVideoCodecs::videoSubTypeByCodec($this->getVideoCodec());
-	}
+    function hasAudio() {
+        return $this->movie->hasAudio();
+    }
+
+    function getDuration() {
+        return $this->movie->getDuration();
+    }
+
+    function getWidth() {
+        return $this->rotation % 180 == 0 ? $this->movie->getFrameWidth() : $this->movie->getFrameHeight();
+    }
+
+    function getHeight() {
+        return $this->rotation % 180 == 0 ? $this->movie->getFrameHeight() : $this->movie->getFrameWidth();
+    }
+
+    function getVideoCodec() {
+        return $this->movie->getVideoCodec();
+    }
+
+    function getAudioCodec() {
+        return $this->movie->getAudioCodec();
+    }
+
+    private function getTempFileName() {
+        return tempnam(sys_get_temp_dir(), "");
+    }
+
+    function saveImageBySecond($filename = NULL, $seconds = 0, $extension = "png", $safeRevertToZero = FALSE) {
+        $filename = $filename == NULL ? $this->getTempFileName() . "." . $extension : $filename;
+        touch($filename);
+        $frameCount = $this->movie->getFrameCount();
+        if ($frameCount > 0) {
+            $frameDuration = $this->getDuration() / $frameCount;
+            if (ceil($seconds / $frameDuration) >= $frameCount)
+                $seconds = ($frameCount - 1) * $frameDuration;
+        }
+        $frame = $this->video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($seconds));
+        if (@$this->rotation && !$this->autorotate)
+            $frame->addFilter(new RotationFrameFilter($this->rotation));
+        $frame->save($filename);
+        if ($safeRevertToZero && !is_file($filename)) {
+            $frame = $this->video->frame(0);
+            if (@$this->rotation && !$this->autorotate)
+                $frame->addFilter(new RotationFrameFilter($this->rotation));
+            $frame->save($filename);
+        }
+        return $filename;
+    }
+
+    function saveImageByPercentage($filename = NULL, $percentage = 0, $extension = "png", $safeRevertToZero = FALSE) {
+        return $this->saveImageBySecond($filename, $percentage * $this->getDuration(), $extension, $safeRevertToZero);
+    }
+
+    function saveAudio($filename = NULL, $extension = "wav") {
+        $filename = $filename == NULL ? $this->getTempFileName() . "." . $extension : $filename;
+        touch($filename);
+        $audio = new FFMpeg\Media\Audio($this->filename, $this->ffmpeg->getFFMpegDriver(), $this->ffmpeg->getFFProbe());
+        $audio->save(new NullAudio(), $filename);
+        return $filename;
+    }
+
+    function getVideoType($filename = NULL) {
+        return FfmpegVideoCodecs::videoTypeByCodecAndFileName($this->getVideoCodec(), @$filename ? $filename : $this->filename);
+    }
+
+    function getVideoSubType() {
+        return FfmpegVideoCodecs::videoSubTypeByCodec($this->getVideoCodec());
+    }
 
     function getAudioSubType() {
         return FfmpegVideoCodecs::audioSubTypeByCodec($this->getAudioCodec());
--- FfmpegVideoTranscoding.php
+++ FfmpegVideoTranscoding.php
@@ -8,67 +8,67 @@ require_once(dirname(__FILE__) . "/FfmpegExtensions.php");
 
 Class FfmpegVideoTranscoding {
 
-	public static $faststart_binary = "qt-faststart";
-	public static $qtrotate_binary = "qtrotate.py";
+    public static $faststart_binary = "qt-faststart";
+    public static $qtrotate_binary = "qtrotate.py";
 
-	public static $ffmpeg_binary = NULL;
+    public static $ffmpeg_binary = NULL;
     public static $ffprobe_binary = NULL;
 
-	/* Returns target file name
-	 *
-	 * Options:
-	 *  - bool replace (optional): Default is true
-	 *  - string target (optional)
-	 */
-	public static function faststart($source, $options = array()) {
-		$target = @$options["target"] ? $options["target"] : tempnam(sys_get_temp_dir(), "");
-		touch($target);
-		$command = self::$faststart_binary . " '" . $source . "' '" . $target . "'";
-		$result = 0;
-		try {
-			exec($command, $output, $result);
-			if ($result != 0)
-				throw new VideoTranscodingException(VideoTranscodingException::FAST_START_EXEC_FAILED);
-			if (file_exists($target) && filesize($target) > 0) {
-				if (!isset($options["replace"]) || $options["replace"]) {
-					if (!rename($target, $source)) {
-						@unlink($target);
-						throw new VideoTranscodingException(VideoTranscodingException::FAST_START_RENAME_FAILED);
-					}
-				}
-			} else {
-				if (isset($options["replace"]) && !$options["replace"]) {
-					if (!rename($source, $target))
-						throw new VideoTranscodingException(VideoTranscodingException::FAST_START_RENAME_FAILED);
-				}
-			}
-			if (!isset($options["replace"]) || $options["replace"])
-				return $source;
-			else
-				return $target;
-		} catch (Exception $e) {
-			throw new VideoTranscodingException(VideoTranscodingException::FAST_START_EXCEPTION, $e->getMessage());
-		}
-	}
+    /* Returns target file name
+     *
+     * Options:
+     *  - bool replace (optional): Default is true
+     *  - string target (optional)
+     */
+    public static function faststart($source, $options = array()) {
+        $target = @$options["target"] ? $options["target"] : tempnam(sys_get_temp_dir(), "");
+        touch($target);
+        $command = self::$faststart_binary . " '" . $source . "' '" . $target . "'";
+        $result = 0;
+        try {
+            exec($command, $output, $result);
+            if ($result != 0)
+                throw new VideoTranscodingException(VideoTranscodingException::FAST_START_EXEC_FAILED);
+            if (file_exists($target) && filesize($target) > 0) {
+                if (!isset($options["replace"]) || $options["replace"]) {
+                    if (!rename($target, $source)) {
+                        @unlink($target);
+                        throw new VideoTranscodingException(VideoTranscodingException::FAST_START_RENAME_FAILED);
+                    }
+                }
+            } else {
+                if (isset($options["replace"]) && !$options["replace"]) {
+                    if (!rename($source, $target))
+                        throw new VideoTranscodingException(VideoTranscodingException::FAST_START_RENAME_FAILED);
+                }
+            }
+            if (!isset($options["replace"]) || $options["replace"])
+                return $source;
+            else
+                return $target;
+        } catch (Exception $e) {
+            throw new VideoTranscodingException(VideoTranscodingException::FAST_START_EXCEPTION, $e->getMessage());
+        }
+    }
 
 
-	/* Returns temporary filename or throws exception
-	 *
-	 * Options:
-	 *  - bool replace (optional): Default is false
-	 *  - string target (optional): Target file name
-	 *  - array filters (optional): Other filters
-	 *  - string format (optional): Mp4 / Flv
-	 *  - width (optional): New width
-	 *  - height (optional): New height
-	 *  - timeout (optional): In seconds. Default is one day.
-	 *  - bool faststart (optional),
-	 *  - int duration (optional)
-	 *  - bool rotate (optional, default false),
-	 *  - bool autorotate
-	 *  - int rotate_add (optional)
-	 *
-	 */
+    /* Returns temporary filename or throws exception
+     *
+     * Options:
+     *  - bool replace (optional): Default is false
+     *  - string target (optional): Target file name
+     *  - array filters (optional): Other filters
+     *  - string format (optional): Mp4 / Flv
+     *  - width (optional): New width
+     *  - height (optional): New height
+     *  - timeout (optional): In seconds. Default is one day.
+     *  - bool faststart (optional),
+     *  - int duration (optional)
+     *  - bool rotate (optional, default false),
+     *  - bool autorotate
+     *  - int rotate_add (optional)
+     *
+     */
     public static function transcode($source, $options) {
         $target = @$options["target"] ? $options["target"] : tempnam(sys_get_temp_dir(), "");
         touch($target);
@@ -141,7 +141,7 @@ Class FfmpegVideoTranscoding {
             if (!@$options["noaudio"])
                 $video->filters()->synchronize();
             if (@$options["filters"])
-                foreach($options["filters"] as $filter)
+                foreach ($options["filters"] as $filter)
                     $video->addFilter($filter);
             if (@$options["noaudio"])
                 $video->addFilter(new VideoOnlyFilter());
@@ -158,8 +158,7 @@ Class FfmpegVideoTranscoding {
                     $format->setExtraParams(array("-f", "flv", "-ar", "44100"));
                 } else
                     throw new VideoTranscodingException(VideoTranscodingException::TRANSCODE_UNKNOWN_TARGET_FORMAT, $options["format"]);
-            }
-            else
+            } else
                 $format = new ExtraParamsDefaultVideo();
             $video->save($format, $target);
         } catch (Exception $e) {
@@ -206,56 +205,56 @@ Class FfmpegVideoTranscoding {
         }
     }
 
-	private static function extractAudio($source, $format) {
-		try {
-			$target = tempnam(sys_get_temp_dir(), "") . "." . $format;
+    private static function extractAudio($source, $format) {
+        try {
+            $target = tempnam(sys_get_temp_dir(), "") . "." . $format;
             touch($target);
             $config = array(
-				"timeout" => 60 * 60 * 24
-			);
-			if (self::$ffmpeg_binary)
-				$config["ffmpeg.binaries"] = array(self::$ffmpeg_binary);
+                "timeout" => 60 * 60 * 24
+            );
+            if (self::$ffmpeg_binary)
+                $config["ffmpeg.binaries"] = array(self::$ffmpeg_binary);
             if (self::$ffprobe_binary)
                 $config["ffprobe.binaries"] = array(self::$ffprobe_binary);
-			$ffmpeg = FFMpeg\FFMpeg::create($config);
-			$video = $ffmpeg->open($source);
-			$video->addFilter(new AudioOnlyFilter());
-			$format = new ExtraParamsDefaultVideo();
-			$video->save($format, $target);
-			return $target;
-		} catch (Exception $e) {
-			throw new VideoTranscodingException(VideoTranscodingException::TRANSCODE_EXCEPTION, (string)$e);
-		}
-	}
+            $ffmpeg = FFMpeg\FFMpeg::create($config);
+            $video = $ffmpeg->open($source);
+            $video->addFilter(new AudioOnlyFilter());
+            $format = new ExtraParamsDefaultVideo();
+            $video->save($format, $target);
+            return $target;
+        } catch (Exception $e) {
+            throw new VideoTranscodingException(VideoTranscodingException::TRANSCODE_EXCEPTION, (string)$e);
+        }
+    }
 
-	private static function transcodeAudioVideoSeparately($source, $options) {
-		$audio = NULL;
-		try {
-			$audio = self::extractAudio($source, "aac");
-		} catch (Exception $e) {
-			return self::transcode($source, $options);
-		}
-		try {
-			$video = self::transcode($source, array(
-				"format" => $options["format"],
-				"rotate" => $options["rotate"],
-				"rotate_add" => $options["rotate_add"]
-			));
-			unset($options["rotate"]);
-			unset($options["rotate_add"]);
-			if (!@$options["filters"])
-				$options["filters"] = array();
-			$options["filters"][] = new MapAndMergeFilter($audio, 0, 0);
-			$result = self::transcode($video, $options);
-			@unlink($audio);
-			@unlink($video);
-			return $result;
-		} catch (VideoTranscodingException $e) {
-			@unlink($audio);
-			@unlink($video);
-			throw $e;
-		}
-	}
+    private static function transcodeAudioVideoSeparately($source, $options) {
+        $audio = NULL;
+        try {
+            $audio = self::extractAudio($source, "aac");
+        } catch (Exception $e) {
+            return self::transcode($source, $options);
+        }
+        try {
+            $video = self::transcode($source, array(
+                "format" => $options["format"],
+                "rotate" => $options["rotate"],
+                "rotate_add" => $options["rotate_add"]
+            ));
+            unset($options["rotate"]);
+            unset($options["rotate_add"]);
+            if (!@$options["filters"])
+                $options["filters"] = array();
+            $options["filters"][] = new MapAndMergeFilter($audio, 0, 0);
+            $result = self::transcode($video, $options);
+            @unlink($audio);
+            @unlink($video);
+            return $result;
+        } catch (VideoTranscodingException $e) {
+            @unlink($audio);
+            @unlink($video);
+            throw $e;
+        }
+    }
 
     private static function transcodeAudioVideoSeparately2($source, $options) {
         $audio = NULL;
@@ -288,16 +287,16 @@ Class FfmpegVideoTranscoding {
     }
 
     private static function separateAudioVideoTranscodingRequired($source, $options) {
-		if (strpos($source, ".webm", strlen($source) - strlen(".webm")) !== FALSE) {
-			if (@$options["filters"]) {
-				foreach ($options["filters"] as $filter)
-					if ($filter instanceof MapAndMergeFilter)
-						return FALSE;
-			}
-			return TRUE;
-		}
-		return FALSE;
-	}
+        if (strpos($source, ".webm", strlen($source) - strlen(".webm")) !== FALSE) {
+            if (@$options["filters"]) {
+                foreach ($options["filters"] as $filter)
+                    if ($filter instanceof MapAndMergeFilter)
+                        return FALSE;
+            }
+            return TRUE;
+        }
+        return FALSE;
+    }
 
     public static function transcodeGracefully($source, $options) {
         try {
@@ -315,31 +314,38 @@ Class FfmpegVideoTranscoding {
 
 Class VideoTranscodingException extends Exception {
 
-	const FAST_START_EXEC_FAILED = 1;
-	const FAST_START_RENAME_FAILED = 2;
-	const FAST_START_EXCEPTION = 3;
-	const TRANSCODE_RENAME_FAILED = 4;
-	const TRANSCODE_EXCEPTION = 5;
-	const TRANSCODE_UNKNOWN_TARGET_FORMAT = 6;
-	const QTROTATE_FAILED = 7;
+    const FAST_START_EXEC_FAILED = 1;
+    const FAST_START_RENAME_FAILED = 2;
+    const FAST_START_EXCEPTION = 3;
+    const TRANSCODE_RENAME_FAILED = 4;
+    const TRANSCODE_EXCEPTION = 5;
+    const TRANSCODE_UNKNOWN_TARGET_FORMAT = 6;
+    const QTROTATE_FAILED = 7;
 
-	function __construct($message_id, $data = NULL) {
-		$this->message_id = $message_id;
-		$this->data = $data;
-		parent::__construct($this->formatMessage() . " (" . $message_id . ") - " . $data);
-	}
+    function __construct($message_id, $data = NULL) {
+        $this->message_id = $message_id;
+        $this->data = $data;
+        parent::__construct($this->formatMessage() . " (" . $message_id . ") - " . $data);
+    }
 
-	function formatMessage() {
-		switch ($this->message_id) {
-			case self::FAST_START_EXEC_FAILED : return "Fast Start Exec Failed";
-			case self::FAST_START_RENAME_FAILED : return "Fast Start Rename Failed";
-			case self::FAST_START_EXCEPTION : return "Fast Start Exception";
-			case self::TRANSCODE_RENAME_FAILED : return "Transcode Rename Failed";
-			case self::TRANSCODE_EXCEPTION : return "Transcode Exception";
-			case self::TRANSCODE_UNKNOWN_TARGET_FORMAT : return "Transcode Unknown Target Format";
-			case self::QTROTATE_FAILED : return "Rotate Exec Failed";
-		}
-	}
+    function formatMessage() {
+        switch ($this->message_id) {
+            case self::FAST_START_EXEC_FAILED :
+                return "Fast Start Exec Failed";
+            case self::FAST_START_RENAME_FAILED :
+                return "Fast Start Rename Failed";
+            case self::FAST_START_EXCEPTION :
+                return "Fast Start Exception";
+            case self::TRANSCODE_RENAME_FAILED :
+                return "Transcode Rename Failed";
+            case self::TRANSCODE_EXCEPTION :
+                return "Transcode Exception";
+            case self::TRANSCODE_UNKNOWN_TARGET_FORMAT :
+                return "Transcode Unknown Target Format";
+            case self::QTROTATE_FAILED :
+                return "Rotate Exec Failed";
+        }
+    }
 
 }
 
